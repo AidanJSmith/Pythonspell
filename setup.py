f56f6a40477c8b8c3fb263c60f58cfaa20b4e442
Aidan Smith <lolbot55555@gmail.com>
2020-04-08 08:32:22 -0700

    Fixed Dir Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(  name='pyspell',
+        version='0.1',
+        description='A simple python spellchecker built on BK Trees and Damerau–Levenshtein distance',
+        url='https://github.com/AidanJSmith',
+        author='Aidan Smith',
+        author_email='100023755@mvla.net',
+        license='MIT',
+        packages=['pyspell'],
+        zip_safe=False)
